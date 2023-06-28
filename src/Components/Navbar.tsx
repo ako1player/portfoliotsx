b056952c1c7db9f45c37b0f241f44d3f155e2b56
Adrian Garcia Rios <adrigar10@gmail.com>
2023-06-28 10:14:55 -0400

    update resume

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -2,12 +2,12 @@ import { BsFillMoonStarsFill } from "react-icons/bs";
 
 const Navbar = ({dark} : any) =>{
     return(
-        <nav className='py-10 mb-12 flex justify-between'>
+        <nav className='flex justify-between py-10 mb-12'>
             <h1 className='text-xl text-transparent bg-clip-text bg-gradient-to-r from-violet-500 to-blue-500'>Adrian Garcia Rios</h1>
             <ul className='flex items-center'>
-              <li><BsFillMoonStarsFill className='cursor-pointer text-2xl dark:text-yellow-200' onClick={dark}/></li>
+              <li><BsFillMoonStarsFill className='text-2xl cursor-pointer dark:text-yellow-200' onClick={dark}/></li>
               <li>
-                <a className='bg-gradient-to-r from-violet-500 to-blue-500 text-white px-4 py-2 rounded-md ml-8' href='../Resume/Resume (4).pdf' target="_blank">Resume</a>
+                <a className='px-4 py-2 ml-8 text-white rounded-md bg-gradient-to-r from-violet-500 to-blue-500' href='../Resume/resume.pdf' target="_blank">Resume</a>
               </li>
             </ul>
         </nav>
